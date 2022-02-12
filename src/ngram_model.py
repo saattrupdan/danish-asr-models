f2496289dd9b318b34f159228a0cbe67b45236b2
saattrupdan <saattrupdan@gmail.com>
2022-02-12 00:24:57 +0000

    feat: Add ngram dataset scaffolding

---
+++ src/ngram_model.py
@@ -0,0 +1,50 @@
+'''Language model to boost performance of the speech recognition model'''
+
+from datasets import load_dataset
+from unicodedata import normalize
+from pathlib import Path
+import json
+import re
+
+
+def clean_texts(examples: dict) -> dict:
+    '''Clean the texts of the dataset.
+
+    Args:
+        examples (dict):
+            Examples of the dataset.
+
+    Returns:
+        dict:
+            Cleaned examples of the dataset.
+    '''
+    # Import vocabulary
+    with Path('vocab.json').open() as f:
+        vocab = list(json.load(f).keys())
+
+    # NFKC normalize the transcriptions
+    examples['text'] = normalize('NFKC', examples['text'])
+
+    # Make the text lowercase
+    examples['text'] = examples['text'].lower()
+
+    # Remove all characters that are not in the vocabulary, or are whitespace
+    regex = f'[^{re.escape("".join(vocab))} ]'
+    examples['text'] = re.sub(regex, '', examples['text'])
+
+    # Replace multiple spaces with a single space
+    examples['text'] = re.sub(r' +', ' ', examples['text'])
+
+
+
+def train_ngram_model():
+    '''Trains an ngram language model'''
+
+    # Load the dataset
+    dataset = load_dataset('DDSC/reddit-da', split='train')
+
+    # Preprocess the dataset
+    dataset = dataset.map(clean_texts)
+
+    # Push the preprocessed dataset to the HF Hub
+    dataset.push_to_hub('DDSC/reddit-da-asr-preprocessed')
