c64b99f73535cde60e5ca1dd570f51c217f1c1b5
saattrupdan <saattrupdan@gmail.com>
2022-02-16 23:51:25 +0000

    feat: Set up training

--- src/train.py
+++ src/train.py
@@ -152,25 +152,6 @@ if __name__ == '__main__':
         early_stopping_patience=5
     )
 
-    xlsr_1b_config = Config(
-        pretrained_model_id='facebook/wav2vec2-xls-r-1b',
-        finetuned_model_id='saattrupdan/wav2vec2-xls-r-1b-cv8-da',
-        mask_time_prob=0.075,
-        mask_feature_prob=0.075,
-        learning_rate=4e-5,
-        epochs=500,
-        warmup_steps=500,
-        batch_size=1,
-        gradient_accumulation_steps=32,
-        attention_dropout=0.1,
-        feat_proj_dropout=0.1,
-        hidden_dropout=0.1,
-        final_dropout=0.1,
-        layerdrop=0.1,
-        early_stopping=True,
-        early_stopping_patience=5
-    )
-
     alvenir_config = Config(
         pretrained_model_id='Alvenir/wav2vec2-base-da',
         finetuned_model_id='saattrupdan/alvenir-wav2vec2-base-cv8-da',
@@ -190,4 +171,4 @@ if __name__ == '__main__':
         early_stopping_patience=5
     )
 
-    train(xlsr_1b_config)
+    train(xlsr_300m_config)
