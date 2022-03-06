cd33193a340ce2c8b5134d64fc5dcd8ebc0e70e1
saattrupdan <saattrupdan@gmail.com>
2022-03-06 10:30:41 +0000

    feat: Lower batch size

--- src/train.py
+++ src/train.py
@@ -169,8 +169,8 @@ if __name__ == '__main__':
         test_name='test_balanced',
         pretrained_model_id='facebook/wav2vec2-xls-r-300m',
         finetuned_model_id='saattrupdan/wav2vec2-xls-r-300m-ftspeech',
-        batch_size=8,
-        gradient_accumulation_steps=4,
+        batch_size=4,
+        gradient_accumulation_steps=8,
         learning_rate=1e-4,
         warmup_steps=2000,
         freeze_feature_encoder=True
