691a17ea4177fed544494aa7068b016266b1a24e
saattrupdan <saattrupdan@gmail.com>
2022-03-07 21:14:55 +0000

    fix: dataset = (new dataset)

--- src/data.py
+++ src/data.py
@@ -76,9 +76,9 @@ class AudioDataset:
         self.initialise_preprocessor()
 
         # Preprocess the datasets
-        self.train.map(self._preprocess_one)
-        self.val.map(self._preprocess_one)
-        self.test.map(self._preprocess_one)
+        self.train = self.train.map(self._preprocess_one)
+        self.val = self.val.map(self._preprocess_one)
+        self.test = self.test.map(self._preprocess_one)
 
         return self
 
